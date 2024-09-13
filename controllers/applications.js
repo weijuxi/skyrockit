c1b2aa07b56305a62bfd6a1044df3a43cff0e65b
weijuxi <2568706310@qq.com>
2024-09-13 10:15:59 -0700

    Add delete and shoe page

--- controllers/applications.js
+++ controllers/applications.js
@@ -36,4 +36,29 @@ router.get('/new', async (req, res) => {
 });
 
 
+//show route(/users/:userId/applications/:applicationId)
+router.get('/:applicationId', async (req, res) => {
+    try {
+        const currentUser = await User.findById(req.session.user._id);
+        const application = currentUser.applications.id(req.params.applicationId);
+        res.render('applications/show.ejs', {application: application});
+    } catch (error) {
+        console.log(error, "<-----------------error");
+        res.redirect('/');
+    }
+});
+
+
+router.delete('/:applicationId', async (req, res) => {
+    try {
+        const currentUser = await User.findById(req.session.user._id);
+        currentUser.applications.id(req.params.applicationId).deleteone();
+        await currentUser.save();
+        res.redirect(`/users/${currentUser._id}/applications`);
+    } catch (error) {
+        console.log(error, "<-----------------error");
+        res.redirect('/');
+    }
+});
+
 module.exports = router;
